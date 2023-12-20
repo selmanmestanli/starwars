6f3f58702631bbc4310f03eaf36629a8518bf9cf
selman mestanli <selmanmestanli@gmail.com>
2023-12-20 10:36:37 +0300

    create initial component function for characters page

---
+++ pages/characters/index.js
@@ -0,0 +1,9 @@
+export default function AllCharacters({ characters }) {
+  return (
+    <div className="mx-auto pt-6 lg:pt-16">
+      <h1 className="text-lg md:text-xl lg:text-2xl text-center">
+        Explore and Select Your Squad
+      </h1>
+    </div>
+  );
+}
