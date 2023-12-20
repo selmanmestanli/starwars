fce811f559c4b078d34220a8b48d8a328205e6bc
selman mestanli <selmanmestanli@gmail.com>
2023-12-20 11:20:04 +0300

    add the ui structure for details page

--- pages/characters/[charId].js
+++ pages/characters/[charId].js
@@ -1,4 +1,29 @@
-export default function CharacterDetailsPage() {}
+import client from '@/apolloClient';
+import { gql } from '@apollo/client';
+import Link from 'next/link';
+
+export default function CharacterDetailsPage({ characterDetails }) {
+  const { name, height, mass, image } = characterDetails;
+  return (
+    <template className="flex flex-col gap-8 m-16 mx-auto md:ml-8 py-8 px-6 md:p-16 lg:p-32 border-2 w-fit shadow-md shadow-gray-50 rounded-lg">
+      <div>
+        <h1 className="text-base md:text-lg pb-6">{name}'s Profile</h1>
+        <img src={image} alt={`details-${name}`} />
+      </div>
+      <div>
+        {name && <p>Name: {name}</p>}
+        {height && <p>Height: {height}</p>}
+        {mass && <p>Mass: {height}</p>}
+      </div>
+      <Link
+        href="/characters"
+        className="bg-gray-300 text-black text-center rounded-md"
+      >
+        Back
+      </Link>
+    </template>
+  );
+}
 
 // make a seperate async getData function to query one time that returned promise can be used in both getStaticProps and getStaticPath
 async function getData() {
