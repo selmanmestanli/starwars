49464d2ab59d480251166b61919ae397d596869a
selman mestanli <selmanmestanli@gmail.com>
2023-12-20 10:54:18 +0300

    query apollo client db in async getData function

--- pages/characters/[charId].js
+++ pages/characters/[charId].js
@@ -1,1 +1,20 @@
 export default function CharacterDetailsPage() {}
+
+// make a seperate async getData function to query one time that returned promise can be used in both getStaticProps and getStaticPath
+async function getData() {
+  const { data } = await client.query({
+    query: gql`
+      query Characters {
+        allPeople {
+          id
+          name
+          gender
+          height
+          mass
+          image
+        }
+      }
+    `,
+  });
+  return data;
+}
