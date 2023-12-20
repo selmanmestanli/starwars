d6bb0eb0ff857827c1f31ba555ec9d9e8fb70f96
selman mestanli <selmanmestanli@gmail.com>
2023-12-20 10:26:22 +0300

    create new home page

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+function HomePage() {
+  return (
+    <>
+      <div className="pr-40 pt-40 pl-20 mb-4">
+        <span>
+          Explore the Star Wars Characters <br />
+          and Create Your Own Squad! <br />
+        </span>
+      </div>
+      <img src="/images/background-homepage.jpg" alt="bg-homepage"></img>
+      {/* <div>Authentication From will be Here!</div> */}
+    </>
+  );
+}
+
+export default HomePage;
