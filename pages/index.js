06eca027540540b1f8ae4b92684cd4e219de3d04
selman mestanli <selmanmestanli@gmail.com>
2023-12-20 10:49:34 +0300

    apply getStaticProps function returning data prop

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,38 @@
-import Link from 'next/link';
+import client from '@/apolloClient';
+import { gql } from '@apollo/client';
 
-function HomePage() {
+import CardGrid from '@/components/cards/card-grid';
+
+export default function AllCharacters({ characters }) {
   return (
-    <>
-      <div className="pr-40 pt-40 pl-20 mb-4">
-        <span>
-          Explore the Star Wars Characters <br />
-          and Create Your Own Squad! <br />
-        </span>
-        <Link href="">
-          <button className="text-4xl mt-4 p-2 border border-r-2 rounded-lg cursor-pointer">
-            Let's Start!
-          </button>
-        </Link>
-      </div>
-      <img src="/images/background-homepage.jpg" alt="bg-homepage"></img>
-      {/* <div>Authentication From will be Here!</div> */}
-    </>
+    <div className="mx-auto pt-6 lg:pt-16">
+      <h1 className="text-lg md:text-xl lg:text-2xl text-center">
+        Explore and Select Your Squad
+      </h1>
+      <CardGrid items={characters} />
+    </div>
   );
 }
 
-export default HomePage;
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+      query Characters {
+        allPeople {
+          id
+          name
+          gender
+          height
+          mass
+          image
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      characters: data?.allPeople,
+    },
+  };
+}
