ce01a1e67ce068e06a33b46ad53af16faf05cfc6
selman mestanli <selmanmestanli@gmail.com>
2023-12-20 10:17:58 +0300

    wrap the root next component with Apollo provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import { ApolloProvider } from '@apollo/client';
+import client from '../apolloClient';
+
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
